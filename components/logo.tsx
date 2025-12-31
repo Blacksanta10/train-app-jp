c82454640a2521791fccbf2102611cfd8ccdf856
Demitrius Webb <moutonwebb29@gmail.com>
2025-12-31 17:49:11 -0500

    Created a component to hold any logos I want to use. So far I only use the train emoji

---
+++ components/logo.tsx
@@ -0,0 +1,14 @@
+/**
+ * This contains diffrent logos :))
+ */
+
+
+// Train emoji haha
+export function Logo({ className = "" }: { className?: string }) {
+  return (
+    <div
+      className={`flex items-center justify-center font-bold text-xl ${className}`}>
+      🚄
+    </div>
+  );
+}
