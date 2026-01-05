610da44395a1886dfe69e881a269685181b9e801
Blacksanta10 <moutonwebb29@gmail.com>
2026-01-04 19:09:29 -0500

    setup the infastructure to change the colors of important components in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      
+      /**
+       * These only hold the most important and most reused colors
+       * bg-(any-color defined)
+       */
+      colors: {
+        background: "var(--color-bg)",
+        foreground: "var(--color-text)",
+        muted: "var(--color-text-muted)",
+        border: "var(--color-border)",
+
+        primary: "var(--color-primary)",
+        error: "var(--color-error)",
+      }
+    },
   },
   plugins: [],
 }
