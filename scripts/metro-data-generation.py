18249e042169f32be36f4d40f082e564d85d9ff3
Blacksanta10 <moutonwebb29@gmail.com>
2026-01-07 01:32:46 -0500

    Finished Mita lines data

--- scripts/metro-data-generation.py
+++ scripts/metro-data-generation.py
@@ -7,7 +7,7 @@ import pandas as pd
 # 1. Define the Line Metadata (The "Rules" for your lines)
 line_metadata = {
     # --- TOKYO METRO ---
-    'Asakusa': {'code_letter': 'G', 'color': '#FF9500', 'owner': 'Tokyo Metro'},
+    'Ginza': {'code_letter': 'G', 'color': '#FF9500', 'owner': 'Tokyo Metro'},
     'Marunouchi': {'code_letter': 'M', 'color': '#F62E36', 'owner': 'Tokyo Metro'},
     'Hibiya': {'code_letter': 'H', 'color': '#B5B5AC', 'owner': 'Tokyo Metro'},
     'Tozai': {'code_letter': 'T', 'color': '#009BBF', 'owner': 'Tokyo Metro'},
@@ -27,7 +27,7 @@ line_metadata = {
 # 2. Your Station Data (The "Input")
 # Format: [Station Name, Kanji, Line Name, Station Number, Is Interchange(1=multiple-lines)]
 raw_data = [
-    
+
     # Asakusa Line's Stations (20 stations)
     ['Nishi-magome', '西馬込', 'Asakusa', '01', 0],
     ['Magome', '馬込', 'Asakusa', '02', 0],
@@ -49,6 +49,37 @@ raw_data = [
     ['Asakusa', '浅草', 'Asakusa', '18', 1],
     ['Honjo-azumabashi', '本所吾妻橋', 'Asakusa', '19', 0],
     ['Oshiage', '押上', 'Asakusa', '20', 0],
+
+    # Mita Line's Stations (27 stations)
+    ['Meguro','目黒', 'Mita', '01', 1],
+    ['Shirokanedai','白金台', 'Mita', '02', 1],
+    ['Shirokane-takanawa','白金高輪', 'Mita', '03', 1],
+    ['Mita','三田', 'Mita', '04', 1],
+    ['Shibakoen','芝公園', 'Mita', '05', 0],
+    ['Onarimon','御成門', 'Mita', '06', 0],
+    ['Uchisaiwaicho','内幸町', 'Mita', '07', 0],
+    ['Hibiya','日比谷', 'Mita', '08', 1],
+    ['Otemachi','大手町', 'Mita', '09', 1],
+    ['Jimbocho','神保町', 'Mita', '10', 1],
+    ['Suidobashi','水道橋', 'Mita', '11', 1],
+    ['Kasuga','春日', 'Mita', '12', 1],
+    ['Hakusan','白山', 'Mita', '13', 0],
+    ['Sengoku','千石', 'Mita', '14', 0],
+    ['Sugamo','巣鴨', 'Mita', '15', 1],
+    ['Nishi-sugamo','西巣鴨', 'Mita', '16', 1],
+    ['Shin-Itabashi','新板橋', 'Mita', '17', 1],
+    ['Itabashi-kuyakushomae','板橋区役所前', 'Mita', '18', 0],
+    ['Itabashi-honcho','板橋本町', 'Mita', '19', 0],
+    ['Motohasunuma','本蓮沼', 'Mita', '20', 0],
+    ['Shimura-sakaue','志村坂上', 'Mita', '21', 0],
+    ['Shimura-sanchome','志村三丁目', 'Mita', '22', 0],
+    ['Hasune','蓮根', 'Mita', '23', 0],
+    ['Nishidai','西台', 'Mita', '24', 0],
+    ['Takashimadaira','高島平', 'Mita', '25', 0],
+    ['Shin-takashimadaira','新高島平', 'Mita', '26', 0],
+    ['Nishi-takashimadaira','西高島平', 'Mita', '27', 0],
+    
+    
 
 ]
 
